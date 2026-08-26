b89ce16a06628c8d3f674fb86993596f6784d72c
Riyadulislamratul <riadul1810833319@gmail.com>
2026-08-26 11:20:17 +0600

    Transactions page created

---
+++ src/pages/Transactions.jsx
@@ -0,0 +1,8 @@
+export default function Transactions() {
+  return (
+    <div className="page-placeholder">
+      <h2>Transactions</h2>
+      <p>Transaction management will be built here.</p>
+    </div>
+  );
+}
