303e67429a8e4d0d45477cb40dc60d2cb5d7932b
Riyadulislamratul <riadul1810833319@gmail.com>
2026-08-26 11:21:13 +0600

    Accounts page created

---
+++ src/pages/Accounts.jsx
@@ -0,0 +1,8 @@
+export default function Accounts() {
+  return (
+    <div className="page-placeholder">
+      <h2>Accounts</h2>
+      <p>Cash, Bank and Mobile Banking accounts will be managed here.</p>
+    </div>
+  );
+}
