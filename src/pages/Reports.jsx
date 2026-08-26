1ba5f16f7fd8591fb8ec450abaec513e4f6a8308
Riyadulislamratul <riadul1810833319@gmail.com>
2026-08-26 11:21:56 +0600

    Reports Page Created

---
+++ src/pages/Reports.jsx
@@ -0,0 +1,8 @@
+export default function Reports() {
+  return (
+    <div className="page-placeholder">
+      <h2>Reports</h2>
+      <p>Daily, monthly and yearly reports will be built here.</p>
+    </div>
+  );
+}
