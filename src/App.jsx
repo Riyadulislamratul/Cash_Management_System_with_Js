ef71bbcec3841f1c6a88aab321b131be6ba410e2
Riyadulislamratul <riadul1810833319@gmail.com>
2026-08-26 11:35:13 +0600

    App.jsx updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Layout from "./components/layout/Layout";
+
 import Dashboard from "./pages/Dashboard";
 import Transactions from "./pages/Transactions";
 import Accounts from "./pages/Accounts";
@@ -13,10 +14,22 @@ export default function App() {
       <Layout>
         <Routes>
           <Route path="/" element={<Dashboard />} />
-          <Route path="/transactions" element={<Transactions />} />
-          <Route path="/accounts" element={<Accounts />} />
-          <Route path="/reports" element={<Reports />} />
-          <Route path="/settings" element={<Settings />} />
+          <Route
+            path="/transactions"
+            element={<Transactions />}
+          />
+          <Route
+            path="/accounts"
+            element={<Accounts />}
+          />
+          <Route
+            path="/reports"
+            element={<Reports />}
+          />
+          <Route
+            path="/settings"
+            element={<Settings />}
+          />
         </Routes>
       </Layout>
     </BrowserRouter>
