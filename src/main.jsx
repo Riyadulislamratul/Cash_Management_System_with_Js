21bf6c100643af18c7274c0f879230958ffda365
Riyadulislamratul <riadul1810833319@gmail.com>
2026-08-26 11:35:49 +0600

    Main.jsx updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
+import App from "./App";
+import { CashProvider } from "./context/CashContext";
+
+import "./index.css";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <CashProvider>
+      <App />
+    </CashProvider>
   </StrictMode>,
-)
+);
