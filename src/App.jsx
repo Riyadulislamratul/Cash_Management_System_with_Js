e0f7704d09825c128b74d85ce750af5de4c8b33e
Riyadulislamratul <riadul1810833319@gmail.com>
2026-08-26 11:25:35 +0600

    App.jsx updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const App = () => {
+import Layout from "./components/layout/Layout";
+import Dashboard from "./pages/Dashboard";
+import Transactions from "./pages/Transactions";
+import Accounts from "./pages/Accounts";
+import Reports from "./pages/Reports";
+import Settings from "./pages/Settings";
+
+export default function App() {
   return (
-    <div>App</div>
-  )
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/transactions" element={<Transactions />} />
+          <Route path="/accounts" element={<Accounts />} />
+          <Route path="/reports" element={<Reports />} />
+          <Route path="/settings" element={<Settings />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  );
 }
-
-export default App
