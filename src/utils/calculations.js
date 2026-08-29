dceb544ba0878579e48c810e1a7375c24059a541
Riyadulislamratul <riadul1810833319@gmail.com>
2026-08-29 22:26:45 +0600

    Updated Calculations

--- src/utils/calculations.js
+++ src/utils/calculations.js
@@ -5,3 +5,55 @@ export function formatCurrency(amount) {
     maximumFractionDigits: 0,
   }).format(Number(amount) || 0);
 }
+
+export function calculateTotals(transactions) {
+  const income = transactions
+    .filter((transaction) => transaction.type === "income")
+    .reduce(
+      (total, transaction) =>
+        total + Number(transaction.amount),
+      0,
+    );
+
+  const expense = transactions
+    .filter((transaction) => transaction.type === "expense")
+    .reduce(
+      (total, transaction) =>
+        total + Number(transaction.amount),
+      0,
+    );
+
+  return {
+    income,
+    expense,
+    balance: income - expense,
+  };
+}
+
+export function calculateAccountBalances(
+  transactions,
+) {
+  const balances = {
+    Cash: 0,
+    Bank: 0,
+    "Mobile Banking": 0,
+  };
+
+  transactions.forEach((transaction) => {
+    const account = transaction.account;
+
+    if (!balances.hasOwnProperty(account)) {
+      return;
+    }
+
+    const amount = Number(transaction.amount) || 0;
+
+    if (transaction.type === "income") {
+      balances[account] += amount;
+    } else if (transaction.type === "expense") {
+      balances[account] -= amount;
+    }
+  });
+
+  return balances;
+}
